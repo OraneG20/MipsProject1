9ad0d4594e973edef13f01c43ac439b5da1bed86
OraneG20 <44150109+OraneG20@users.noreply.github.com>
2018-10-15 20:39:32 -0400

    Successfully printed 2 letters, loading $a0 once

--- mips1.asm
+++ mips1.asm
@@ -2,5 +2,14 @@
 .text
 
 main:
+	
+	li $v0,11
+    
+	la $a0, 79 # O
+	syscall
+    
+	addi $a0, $a0, 35 # r (114)
+	syscall
+    
 	li $v0, 10	# calls the system exit function
 	syscall		# OS perform execution
