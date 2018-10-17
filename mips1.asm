1d6828101e288372d5c1076fc296f88f1368c61d
OraneG20 <44150109+OraneG20@users.noreply.github.com>
2018-10-17 00:00:00 -0400

    Print ID digits

--- mips1.asm
+++ mips1.asm
@@ -4,8 +4,22 @@
 main:
 	
 	li $v0,11
-    
-	la $a0, 79 # O
+    	
+	la $a0, 64 # @ (64)
+        syscall
+
+        li $v0, 1
+
+        addi $a0, $a0, -64 # 0
+        syscall
+
+	addi $a0, $a0, 2 # 2
+        syscall
+
+	addi $a0, $a0, 6 # 8
+        syscall
+
+	addi $a0, $a0, 71 # O
 	syscall
     
 	addi $a0, $a0, 35 # r (114)
